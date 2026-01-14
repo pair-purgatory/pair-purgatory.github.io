0fb4ea2d9f711391a19a63c10776704ee3e4a81c
Hazel <141371172+chiptumor@users.noreply.github.com>
2026-01-14 13:19:27 -0600

    generic save

--- build.js
+++ build.js
@@ -15,6 +15,33 @@ FileSystem.readdir(story).then(async dirs => {
     const regex = /\{\{\s*([\w.]+)\s*\}\}/g;
 
     for (const dir of dirs) {
+        const replace = {};
 
+        const meta = await (
+            Glob(Path.join(story, dir, "metadata.yaml"))
+            .then(path => FileSystem.readFile(path, "utf8"))
+            .then(file => YAML.parse(file))
+        );
+
+        replace.meta = {
+            series: "Pair Purgatory",
+            title: meta.title,
+            author: meta.author
+        };
+
+        replace.page = {
+            styles: "<!-- styles go here -->",
+            scripts: "<!-- scripts go here -->"
+        };
+
+        Promise.all(meta.content.map(item => FileSystem.readFile(
+            Path.join(story, dir, "content", item)
+        ))).then(
+            content => replace.content = content.join("\n")
+        );
+
+        const page = template.replace(regex, (match, path) => {
+            // https://github.com/chiptumor/chiptumor.github.io/blob/main/build/plugin/replace-templates.js
+        });
     }
 });
