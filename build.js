6341eeaed71fe3bfbd2d4b3f9d9b0aa1e191cff0
Hazel <141371172+chiptumor@users.noreply.github.com>
2026-01-14 12:42:21 -0600

    generic save

--- build.js
+++ build.js
@@ -2,7 +2,19 @@ import * as Path from "node:path";
 import { glob as Glob } from "glob";
 import * as FileSystem from "node:fs/promises";
 
-Glob("./src/story/*/").then(async files => {
-    const episodeTemplate = await FileSystem.readFile("./include/episode-template.html", "utf8");
-    
+import YAML from "yaml";
+
+const dist = Path.resolve("./dist/");
+await FileSystem.mkdir(dist);
+
+const story = Path.resolve("./src/story/");
+FileSystem.readdir(story).then(async dirs => {
+    const template = await FileSystem.readFile(
+        Path.resolve("./include/episode-template.html"),
+    "utf8");
+    const regex = /\{\{\s*([\w.]+)\s*\}\}/g;
+
+    for (const dir of dirs) {
+
+    }
 });
